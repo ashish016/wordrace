d33d593ce915ec8899970514b44ea2b2cd5079f4
ashish016 <ashishdash016@gmail.com>
2021-09-08 18:03:27 +0530

    .

--- WordRace_Backend/urls.py
+++ WordRace_Backend/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 urlpatterns = [
-    path('api/leaderboard/', include('leaderboard.urls'))
+    path('api/leaderboard/', include('leaderboard.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
